c8ef67b055d3b3911cc762bcbf9f1a947515a96a
Milton Madanda <madandat@gmail.com>
2015-04-22 08:39:20 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-livechat',
-    version='1.0.0',
+    version='1.0.1',
     description='Very Simple Live Chat Django Application',
     long_description = open('README.md', 'r').read() + \
             open('AUTHORS.md', 'r').read() + open('CHANGELOG.md', 'r').read(),
